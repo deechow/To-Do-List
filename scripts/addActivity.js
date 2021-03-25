1f4e4a139d3360eb2b454442779f5b3e738a068b
deechow <79235633+deechow@users.noreply.github.com>
2021-03-25 13:52:55 -0400

    Notes and Coding Updates
    Vincent helped me with the coding, and I used the notes to decipher what he did. Instead of creating new lines by inserting it blindly, he showed me to utilizes functions and to create things. Such as drawtable to rewrite the codes in index when clicking addActivity, creating the activity list, and anchoring checkboxes.

--- scripts/addActivity.js
+++ scripts/addActivity.js
@@ -1,22 +1,54 @@
-/* 
-  Add your activity to this list
-*/
-const activity = [
-    {
-        id: 1,
-        task: Coding,
-        description: Question 3,
-    },
-    {
-        id: 2,
-        task: Question 3,
-        description: Create a checklist where you can add more,
-    },
-];
-
-const ID = "id";
-const TASK = "task";
-const DESCRIPTION = "description";
-
-const fields = [STATUS, TASK, DESCRIPTION];
-const table = document.getElementById("checkList");
+    // Add your activity to this list
+    let activity = [];
+    // Find the table to interact with
+    const table = document.getElementById("checkList");
+    // Creating a checkbox
+    function drawCheckBox(item, index) {
+        let checked = ""
+        if (item.isComplete) {
+          checked = "checked";
+        }
+        // Inserting the checkbox with its asigned index to differienate new ones, creating it with clickedCheckBox function in mind
+        // Create checkbox, name the id with the index, and updating the checked status in the list
+        return `<input type='checkbox' id=${`check-${index}`} ${checked} onclick="clickedCheckBox(${index})"></input>`;
+        // if activity[index].isComplete = 'checked' {
+        //     return `<div class='completed></div>`
+        // }
+    }
+    // Anchoring the checkbox when adding new activities by updating the list
+    function clickedCheckBox(index) {
+        activity[index].isComplete = !activity[index].isComplete;
+    }
+    // add a feature to change the style for completed task
+    // function checkedCheckBox(index) {
+    //     if activity[index].isComplete = 'checked' {
+    //         innerHTML += `<div class='completed></div>`
+    //     }
+    // }
+    // Creating the table. It creates a new table each time, deleting the old one
+    function drawTable() {
+        let tbodyRef = table.getElementsByTagName('tbody')[0];
+        // reset table
+        tbodyRef.innerHTML = "";
+        //search each activity from the list, and then add it accordingly to the row
+        activity.forEach((item, index) => {
+            const row = tbodyRef.insertRow(index);
+            let innerHTML = "";
+            innerHTML += `<th scope='row'>${drawCheckBox(item, index)}</th>`;
+            innerHTML += `<td>${item.task}</td>`;
+            innerHTML += `<td>${item.description}</td>`;
+            row.innerHTML = innerHTML;
+        });
+    }
+  
+    // add Activity onto the list when it is clicked
+    function addActivity() {
+        // grab the value of the inputs
+        const task = document.getElementById("TASK").value;
+        const description = document.getElementById("DESCRIPTION").value;
+        activity.push({ isComplete: false, task: task, description: description })
+        // Create the new table
+        drawTable();
+    }
+    // Create the table at start
+    drawTable();
